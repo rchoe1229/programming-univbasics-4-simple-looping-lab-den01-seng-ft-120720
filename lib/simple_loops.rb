56b5ae66bc841008f322bf0e7de858611683ed8d
Ryan Choe <ryan.choe@outlook.com>
2020-11-24 20:08:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,12 @@ def loop_message_n_times(message, limit)
   counter += 1
 end
 end
+
+def output_array(array)
+  counter = 0 
+  
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+end 
+end
